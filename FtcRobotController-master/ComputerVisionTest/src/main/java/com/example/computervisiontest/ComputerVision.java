fdbdd8861f444dbbb18858e18b85440cacb964ce
tatooine12201 <tatooine12201@gmail.com>
2023-10-16 16:04:06 +0300

    Add examples

---
+++ FtcRobotController-master/ComputerVisionTest/src/main/java/com/example/computervisiontest/ComputerVision.java
@@ -0,0 +1,17 @@
+package com.example.computervisiontest;
+
+//import org.opencv.core.Core;
+//import org.opencv.core.Mat;
+//import org.opencv.imgcodecs.Imgcodecs;
+///public class ComputerVision {
+   // public static void main(String[] args)
+   // {
+      //  System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+        //Imgcodecs imageCodecs = new Imgcodecs();
+      //  String file ="C:\Users\tatoo\Desktop\test1.jpg";
+       // Mat matrix = imageCodecs.imread(file);
+       // System.out.println("("Image Loaded");
+   // }
+//}
+
+// "C:\Users\tatoo\Desktop\test1.jpg"
